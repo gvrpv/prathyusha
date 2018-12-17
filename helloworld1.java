a92490411ac1bb9e8a712af77876be6fb2fa70f2
gvrpv <vaishug963@gmail.com>
2018-12-17 07:30:31 +0000

    commiting after modifying

--- helloworld1.java
+++ helloworld1.java
@@ -2,6 +2,6 @@ public class helloworld1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World 1");
+		System.out.println("Hello World1!");
 	}
 	}
--- helloworld2.java
+++ helloworld2.java
@@ -2,6 +2,6 @@ public class helloworld2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World 2");
+		System.out.println("Hello World2!");
 	}
 	}
